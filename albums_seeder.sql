642e44b1f10b9cf15bdbed398fb0d284ce984deb
Evan Williams <evan.b.williams95@gmail.com>
2021-06-18 10:43:50 -0500

    added album_seeder

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller','1982','47.3','Pop, funk, rock'),
+       ('Whitney Houston ', 'The Bodyguard','1992','32.4','Pop, Soul, R&B'),
+       ('Eagles', 'Their Greatest Hits (1971–1975)','1976','41.2','Country Rock, soft rock, folk rock'),
+       ('Eagles', 'Hotel California','1976','31.5','soft rock'),
+       ('Adele', '21','2011','30.8','Pop, soul')
