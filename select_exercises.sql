08297f49c2a42bf7f7cb76b58f52c73eb997f382
Evan Williams <evan.b.williams95@gmail.com>
2021-06-18 16:21:03 -0500

    Finished walkthrough for albums

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 
 -- The name of all albums by Pink Floyd.
-SELECT name FROM albums WHERE artist = 'Pink FLoyd';
+SELECT name AS 'The name of all Pink Floyd albums' FROM albums WHERE artist = 'Pink FLoyd';
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band release date' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
@@ -10,7 +10,7 @@ SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band release date' FRO
 SELECT genre AS 'Nirvana''s Nevermind Genre' FROM albums WHERE name = 'Nevermind';
 
 -- Which albums were released in the 1990s
-SELECT name AS 'Albums released in the 90''s' FROM albums WHERE release_date BETWEEN '1990' AND '1999';
+SELECT name AS 'Albums released in the 90''s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales
 SELECT name AS 'Albums with less than 20 mil. sales' FROM albums WHERE sales <= 20.0;
